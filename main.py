9e4b627e47d2cbb9db88ace33dff42d1e3f0ef7d
Radmir <100975073+Conqueror1254@users.noreply.github.com>
2023-05-30 20:31:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,22 +21,7 @@ def ticker():
     label.pack()
     w.mainloop()
 
-def videomake():
-    window_name = "tk"
-    fourcc = cv2.VideoWriter_fourcc(*"XVID")
-    fps = 30.0
-    record_sec = 3
-    win = gw.getWindowsWithTitle(window_name)[0]
-    win.activate()
-    out = cv2.VideoWriter('video.avi', fourcc, fps, tuple(win.size))
-    for i in range(int(record_sec*fps)):
-        img = pyautogui.screenshot(region=(win.left, win.top, win.width, win.height))
-        frame = np.array(img)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        out.write(frame)
-        cv2.imshow("screenshot", frame)
-        if cv2.waitKey(1) == 'q':
-            break
+
 
 
 
